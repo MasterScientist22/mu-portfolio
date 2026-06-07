00d004f67986b296eb3cb7d789a7b00c2af2c6d5
Tashi <116861179+MasterScientist22@users.noreply.github.com>
2026-06-08 00:31:03 +0600

    Update portfolio data from admin

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -26,7 +26,7 @@
 			{
 				"title": "Photo 1",
 				"tag": "",
-				"description": "I took this photo to capture the beauty of Thimphu from a higher viewpoint. From here, I could see how the city stretches across the valley, with buildings nestled among the hills. The road running through the center adds a sense of direction and movement, while the surrounding greenery highlights Bhutan's natural beauty. To me, this image reflects the peaceful blend of urban development and nature that makes Thimphu unique. great view",
+				"description": "I took this photo to capture the beauty of Thimphu from a higher viewpoint. From here, I could see how the city stretches across the valley, with buildings nestled among the hills. The road running through the center adds a sense of direction and movement, while the surrounding greenery highlights Bhutan's natural beauty. To me, this image reflects the peaceful blend of urban development and nature that makes Thimphu unique.",
 				"year": "",
 				"thumb": "images/fulls/Exhibition photo 1_kelxang.jpg",
 				"full": "images/fulls/Exhibition photo 1_kelxang.jpg",
